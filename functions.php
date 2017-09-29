defce83cd599f61cfd1d7165efc1305e5ec98aa0
David Gaskin <davidburkegaskin@gmail.com>
2017-09-28 23:08:19 -0700

    register footer menu in functions.php

--- functions.php
+++ functions.php
@@ -44,7 +44,8 @@ if ( ! function_exists( 'peak_theme_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'peak-theme' ),
+			'menu-1' => esc_html__( 'Header', 'peak-theme' ),
+                        'menu-2' => esc_html__( 'Footer', 'peak-theme' )
 		) );
 
 		/*

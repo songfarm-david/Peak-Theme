58958c833c1e601e18da6687c5aeef2c89679fe3
David Gaskin <davidburkegaskin@gmail.com>
2017-11-02 19:00:00 -0700

    update hero banner style/css; add page header banner preliminary styles

--- landing-page-home.php
+++ landing-page-home.php
@@ -4,8 +4,8 @@
 
         <div class="hero-headline-container">
             
-            <h1 class="hero-headline" itemprop="headline">Elevate Your Online Potential.</h1>
-            <p class="hero-byline" itemprop="alternativeHeadline">Get a Website and Search Engine Marketing designed to produce more leads and generate more sales for your small to medium-size business.</p>
+            <h1 class="hero-headline hero-text" itemprop="headline"><span>Elevate</span> Your Online Potential.</h1>
+            <p class="hero-byline hero-text" itemprop="alternativeHeadline">Get a Website and Search Engine Marketing designed to produce more leads and generate more sales for your small to medium-size business.</p>
             
             <a href="#">Get Started</a>
             

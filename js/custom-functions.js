6408f9ae3dc84c2766da36d422a448f8c07f26ee
David Gaskin <davidburkegaskin@gmail.com>
2017-11-15 16:04:55 -0800

    add smooth scrolling to homepage

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -1,7 +1,11 @@
-
+/**
+ * 
+ * Custom user-defined functions
+ */
 
 ( function($) {
-   /**
+   
+    /**
      * Apply class to header on scroll
      */
     $(window).scroll(function(){
@@ -12,5 +16,59 @@
            $('.site-header').removeClass('header-sticky');
         }
     }); 
+    
+    /**
+     * Smooth scrolling + focus accessibility and URL updating
+     */
+    // URL updates and the element focus is maintained
+    // originally found via in Update 3 on http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+
+    // filter handling for a /dir/ OR /indexordefault.page
+    function filterPath(string) {
+      return string
+        .replace(/^\//, '')
+        .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+        .replace(/\/$/, '');
+    }
+
+    var locationPath = filterPath(location.pathname);
+    
+    $('a[href*="#"]').each(function () {
+        var thisPath = filterPath(this.pathname) || locationPath;
+        var hash = this.hash;
+        if ($ ("#" + hash.replace(/#/, '')).length) {
+            if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+                var $target = $(hash), target = this.hash;
+                if (target) {
+                    $(this).click(function (event) {
+                        event.preventDefault();
+                        
+                        // if desktop viewport
+                        var scrollToPosition;
+                  
+                        if ( $(window).width() > 1200 ) {
+                            scrollToPosition = $(target).offset().top - 100;
+                        } else if ( $(window).width() > 768 ) {
+                            scrollToPosition = $(target).offset().top - 100;
+                        } else {
+                            scrollToPosition = $(target).offset().top - 145;
+                        }
+                        
+                        $('html, body').animate({scrollTop: scrollToPosition}, 1250, function () {
+                            
+                            $target.focus();
+                            if ($target.is(":focus")){ //checking if the target was focused
+                                return false;
+                            }else{
+                                $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
+                                $target.focus(); //Setting focus
+                            };
+                            //location.hash = target; causes screen jump
+                        });       
+                    });
+                }
+            }
+        }
+    });
     
 })( jQuery );

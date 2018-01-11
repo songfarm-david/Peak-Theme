10b8b5c60d0ab83422c45b8f9278cc6f42fe892c
David Gaskin <davidburkegaskin@gmail.com>
2018-01-10 17:09:07 -0800

    add feature icons to project; add new include file: features-content-creation

---
+++ inc/custom/features-content-creation.php
@@ -0,0 +1,76 @@
+
+<?php include( get_template_directory() . '/assets/icons/content-creation-icons-sprite.svg' );?>
+
+<article id="features-content-creation" class="panel-container panel-color">
+    
+    <div class="entry-content">
+        <h2>Features:</h2>
+        
+        <div class="feature-item-container">
+            
+            <article class="feature-item">
+                <h4>Quick Turn-Around</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#car-speedometer" />
+                </svg>
+                <p>Most work is delivered within a few days. For more involved content, we aim to deliver within 10 days.</p>
+            </article>
+            <article class="feature-item">
+                <h4>Variety of Content Types</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#white-sheets" />
+                </svg>
+                <p>Choose from various content types including videos, blog articles, infographics, e-books, and more.</p>
+            </article>
+
+            <article class="feature-item">
+                <h4>Automated Service for Completely Hands-Off</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#hotel-reception-bell" />
+                </svg>
+                <p>For a completely hands-off approach, we'll create a content strategy for you and execute it in line with your goals and vision.</p>
+            </article>
+            <article class="feature-item">
+                <h4>Only Pay for What You Approve</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#pair-of-bills" />
+                </svg>
+                <p>If you don't like the piece of content we've provided, we'll go back to the drawing board until we've got something you do like!</p>
+            </article>
+
+            <article class="feature-item">
+                <h4>Edits Are Always Free</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#pen" />
+                </svg>
+                <p>Need an edit or two before you can approve of a piece of content? No problem. Edits are always on the house.</p>
+            </article>
+            <article class="feature-item">
+                <h4>100% Ownership of Content</h4>
+                <!--image here-->
+                <svg class="feature-icon">
+                    <use xlink:href="#locked-padlock" />
+                </svg>
+                <p>Any content you approve becomes yours to do with as you please including taking all the credit!</p>
+            </article>
+            
+        </div>
+        
+        
+    </div>
+  
+</article>
+
+<article class="hero-call">
+    <div>
+        <h3>Ready to jump-start your online presence?</h3>     
+        <p><a href="<?php echo get_permalink('1722'); ?>">Contact us today</a> and let's start generating more leads and driving more engagement online for your business.</p>
+    </div>
+    
+</article>
+

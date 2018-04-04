b31b32844f051bc644a0f194614b0498665f3d45
David Gaskin <davidburkegaskin@gmail.com>
2018-04-03 20:59:39 -0700

    re-style portfolio examples

--- inc/custom/portfolio-grid.php
+++ inc/custom/portfolio-grid.php
@@ -1,31 +1,35 @@
 <!--Portfolio image panels-->
-<article id="portfolio-grid">
-    <h2>Portfolio projects</h2>
-    <ul>
-        <li id="songfarm" class="item small">
-            <a href="<?php echo get_permalink( '2333' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '2333' ); ?>" class="hover-button">View Project</a>
-        </li>
-        <li id="cirs" class="item small">
-            <a href="<?php echo get_permalink( '2351' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '2351' ); ?>" class="hover-button">View Project</a>
-        </li>
-        <li id="hollis" class="item small">
-            <a href="<?php echo get_permalink( '2344' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '2344' ); ?>" class="hover-button">View Project</a>
-        </li>
-        <li id="coastal" class="item small">
-            <a href="<?php echo get_permalink( '2340' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '2340' ); ?>" class="hover-button">View Project</a>
-        </li>
-        <li id="canoa" class="item large">
-            <a href="<?php echo get_permalink( '2356' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '2356' ); ?>" class="hover-button">View Project</a>
-        </li>
-        <li id="your-website" class="item large">
-            <a href="<?php echo get_permalink( '1722' ); ?>" class="image-hover"></a>
-            <a href="<?php echo get_permalink( '1722' ); ?>" class="hover-button">Your Website Here</a>
-        </li>
-    </ul>
+<article id="portfolio-grid" class="panel-container panel-color">
+    <div class="entry-content">
+       <h2>Website Examples:</h2>
+       <p>Below are a few examples of the websites we've built. Click on an image to learn more about the details behind the project.</p>
+        <ul>
+            <li id="songfarm" class="item small">
+                <a href="<?php echo get_permalink( '2333' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '2333' ); ?>" class="hover-button">View Project</a>
+            </li>
+            <li id="cirs" class="item small">
+                <a href="<?php echo get_permalink( '2351' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '2351' ); ?>" class="hover-button">View Project</a>
+            </li>
+            <li id="hollis" class="item small">
+                <a href="<?php echo get_permalink( '2344' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '2344' ); ?>" class="hover-button">View Project</a>
+            </li>
+            <li id="coastal" class="item small">
+                <a href="<?php echo get_permalink( '2340' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '2340' ); ?>" class="hover-button">View Project</a>
+            </li>
+    <!--        <li id="canoa" class="item large">
+                <a href="<?php echo get_permalink( '2356' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '2356' ); ?>" class="hover-button">View Project</a>
+            </li>
+            <li id="your-website" class="item large">
+                <a href="<?php echo get_permalink( '1722' ); ?>" class="image-hover"></a>
+                <a href="<?php echo get_permalink( '1722' ); ?>" class="hover-button">Your Website Here</a>
+            </li>-->
+        </ul> 
+    </div>
+    
 </article>
 

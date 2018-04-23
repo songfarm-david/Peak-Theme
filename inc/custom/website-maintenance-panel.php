2e5b0b293f1b78b01f5be929ee292d15aabb7a8c
David Gaskin <davidburkegaskin@gmail.com>
2018-04-23 16:04:59 -0700

    no idea

--- inc/custom/website-maintenance-panel.php
+++ inc/custom/website-maintenance-panel.php
@@ -6,22 +6,22 @@
             <div class="step-container">
                 <img class="alignnone size-thumbnail wp-image-2393" src="<?php echo get_template_directory_uri() . '/assets/icons/calendar-color.svg'; ?>" alt="" width="150" height="150" />
 
-                <p>Select the amount of hours that best suit your needs (we will help you decide how many hours you need). Change packages at anytime.</p>
-                <p><a href="https://peakwebsites.ca/wp-content/uploads/2018/04/Peak-Websites-Monthly-Website-Maintenance-Packages-2018.pdf" alt="Monthly Website Maintenance Packages" target="_blank">View Website Maintenance Package details</a></p>
+                <p>Select the amount of hours per month that best suit your online needs (we will help you in determining the optimal number of hours for your situation). You can change packages anytime.</p>
+                <p><a href="https://peakwebsites.ca/wp-content/uploads/2018/04/Peak-Websites-Monthly-Website-Maintenance-Packages-2018.pdf" alt="Monthly Website Maintenance Packages" target="_blank">See Package Information</a></p>
             </div>
             <div class="step-container">
                 <img class="alignnone size-thumbnail wp-image-2393" src="<?php echo get_template_directory_uri() . '/assets/icons/workstation-color.svg'; ?>" alt="" width="150" height="150" />
 
-                <p>We devote those hours to performing essential software and security updates as well as basic SEO. If you have a specific development request, we take care of that request within hours.</p>
+                <p>We devote those hours to performing essential software and security updates, and basic SEO. If there is a development request, expert web developers will make those changes quickly and effectively.</p>
             </div>
             <div class="step-container">
                 <img class="alignnone size-thumbnail wp-image-2393" src="<?php echo get_template_directory_uri() . '/assets/icons/report-color.svg'; ?>" alt="" width="150" height="150" />
 
-                <p>At the end of the month, we provide you with a <em>Website Health and Optimization</em> (WHO) Report, which lets you know how your website is performing as well as any need-to-know information.</p>
+                <p>At regular intervals, we provide you with a high-level performance overview and expert insights in the form of a <em>Website Health and Optimization Report</em> (WHO) that you can use to inform your strategy online.</p>
             </div>
         </div>
-        <p>Our Monthly Website Maintenance Packages are designed to take the stress and worry out of having and maintaining a website. It's like having your own personal Web Developer and SEO in your corner, protecting your asset, and standing by to handle the demands of your online business, promptly and reliably.</p>
-        <p><a href="<?php echo get_permalink( '1722' ); ?>">Contact us today</a> to arrange your Maintenance Package or ask any questions you may have.</p>
+        <p>Our Monthly Website Maintenance Packages are designed to take the stress and worry out of having and maintaining a website; it's like having your own personal Web Developer and SEO in your corner, protecting your asset, and standing by to handle the technical demands of your online business, reliably and on-time.</p>
+        <p><a href="<?php echo get_permalink( '1722' ); ?>">Contact us today</a> to arrange your Maintenance Package or to ask any questions you may have.</p>
     </div>
     
 </section>

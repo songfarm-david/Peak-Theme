5d1fc647654f0f0e311266f42cfca0b096b88ce1
David Gaskin <davidburkegaskin@gmail.com>
2018-08-06 11:07:00 -0700

    remove linked in profile link from footer

--- functions.php
+++ functions.php
@@ -455,3 +455,16 @@ function remove_hentry_class( $classes ) {
     return $classes;
 }
 add_filter( 'post_class', 'remove_hentry_class' );
+
+/**
+ * Remove empty paragraphs created by wpautop()
+ * @author Ryan Hamilton
+ * @link https://gist.github.com/Fantikerz/5557617
+ */
+function remove_empty_p( $content ) {
+	$content = force_balance_tags( $content );
+	$content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
+	$content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
+	return $content;
+}
+add_filter('the_content', 'remove_empty_p', 20, 1);

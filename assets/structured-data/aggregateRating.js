3e236f18afd42cf86134be4737805fc4f21def4b
David Gaskin <davidburkegaskin@gmail.com>
2018-08-14 10:12:50 -0700

    fix homepage review, add link to reviews in site footer

---
+++ assets/structured-data/aggregateRating.js
@@ -0,0 +1,14 @@
+<!-- aggregateRating -->
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org/AggregateRating/",
+	"@type": "AggregateRating",
+	"itemReviewed": {
+		"@type": "Organization",
+		"name": "Peak Websites"
+	},
+	"ratingCount": 19,
+	"reviewCount": "19",
+	"ratingValue": "4.6"
+}
+</script>

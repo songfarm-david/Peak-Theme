891810981c362a50e69d312196ebb287b2f05956
David Gaskin <davidburkegaskin@gmail.com>
2018-08-28 11:12:52 -0700

    comment out active google-analytics code as test to remove local pages from being registered in GA

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -4,8 +4,10 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-gtag('js', new Date());
-gtag('config', 'UA-86141289-1');
+
+// NOTE: commented out for local development
+//gtag('js', new Date());
+//gtag('config', 'UA-86141289-1');
 //# sourceMappingURL=google-analytics.js.map
 //# sourceMappingURL=google-analytics.js.map
 //# sourceMappingURL=google-analytics.js.map
